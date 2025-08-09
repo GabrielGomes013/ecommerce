254016077567ee4ac35b63d23c003437eb9051c9
Gabriel Gomes <goliasgomesco@hotmail.com>
2025-08-09 15:27:22 -0300

    add material there

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+
+const themeDefault = createTheme({
+  palette: {
+    primary: {
+      main: "#000000ff",
+    },
+    secondary: {
+      main: "#ff0000ff",
+    },
+  },
+});
+
 function App() {
-  return <>Olá Mundo</>;
+  return <ThemeProvider theme={themeDefault}></ThemeProvider>;
 }
 
 export default App;
